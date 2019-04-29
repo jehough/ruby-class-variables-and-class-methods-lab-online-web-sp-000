ea6a3a882194f63a93e9f90f906e7ef7e1675a3f
Jim Hough <teachlife@outlook.com>
2019-04-29 15:00:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,13 @@
 class Song
+  attr_accessor :song, :artist, :genre
   @@song_count = 0
   @@song_artist = []
-  @@song_album = []
-  def initialize
+  @@song_genre = []
+  @@genre_count = {}
+  def initialize(song, artist, genre)
+    @song = song
+    @artist = artist
+    @genre = genre
+    @@song_artist << @song
+    @@song_genre << @genre
+    
